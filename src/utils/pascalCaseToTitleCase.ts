593c3181798a6a33f44cf7be05d90329d0dd300e
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2026-01-19 04:46:57 -0800

    Handle more pascal case cases

--- src/utils/pascalCaseToTitleCase.ts
+++ src/utils/pascalCaseToTitleCase.ts
@@ -1,5 +1,9 @@
 export function pascalCaseToTitleCase(str: string): string {
-    return str.replace(/[a-z][A-Z]/g, (match) => {
-        return `${match[0]} ${match[1]}`;
-    });
+    return str
+        .replace(/[a-z][A-Z0-9]/g, (match) => {
+            return `${match[0]} ${match[1]}`;
+        })
+        .replace(/[A-Z]{2,}[a-z0-9]/g, (match) => {
+            return `${match.slice(0, match.length - 2)} ${match.slice(match.length - 2)}`;
+        });
 }
